370bb09a05c1dce9a3be2cf0bb6df29b3a502bf1
Sepehr Aghapour <petrol_cs@yahoo.com>
2025-02-03 12:49:58 +0330

    update Submit button styles for improved disabled state appearance

--- src/components/submit/Submit.tsx
+++ src/components/submit/Submit.tsx
@@ -8,7 +8,7 @@ const SubmitBtn: React.FC<SubmitBtnProps> = ({ className, ...props }) => {
     <div>
       <button
         {...props}
-        className={`${className} bg-blue-500 shadow-md shadow-blue-300/50 text-white hover:shadow-blue-400/40 active:scale-95 active:shadow-md hover:shadow-lg rounded-xl px-2 py-1 cursor-pointer duration-200  hover:text-white disabled:!border-gray-500 disabled:text-gray-500 disabled:hover:text-gray-500 disabled:opacity-70 disabled:hover:bg-transparent`}
+        className={`${className} bg-blue-500 shadow-md shadow-blue-300/50 text-white hover:shadow-blue-400/40 active:scale-95 active:shadow-md hover:shadow-lg rounded-xl px-2 py-1 cursor-pointer duration-200  hover:text-white disabled:hover:shadow-none disabled:text-gray-500 disabled:!bg-gray-300 disabled:hover:scale-100 disabled:hover:text-gray-600 disabled:shadow-none disabled:hover:bg-transparent`}
       >
         <i className="fa-light fa-floppy-disk text-lg"></i>{" "}
         <span className="tracking-[2.5px] text-lg font-bold">SAVE</span>
