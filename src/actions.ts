ccb4b759a20b4319f27ba67eeb2d856e0f3b9c70
Mahdyar Hasanpour <git@mahdyar.me>
2025-02-03 22:25:04 +0330

    fix: add `/` to the api path
    To fix 307 redirect errors

--- src/actions.ts
+++ src/actions.ts
@@ -12,7 +12,7 @@ type createPasteTypes = (
 
 export const createPaste: createPasteTypes = async (paste, slang, password) => {
   const API_URL = import.meta.env.VITE_API_URL as string;
-  const response = await axios.post(API_URL, { paste, slang, password });
+  const response = await axios.post(`${API_URL}/`, { paste, slang, password });
 
   return {
     status: response.status,
