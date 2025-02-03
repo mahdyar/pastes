fce4e9d747ca64fc145f4570f745184c63d6218e
Sepehr Aghapour <petrol_cs@yahoo.com>
2025-02-03 13:39:15 +0330

    style: update Submit button layout and text styling

--- src/components/submit/Submit.tsx
+++ src/components/submit/Submit.tsx
@@ -5,13 +5,13 @@ interface SubmitBtnProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 const SubmitBtn: React.FC<SubmitBtnProps> = ({ className, ...props }) => {
   return (
-    <div>
+    <div className="flex items-center">
       <button
         {...props}
-        className={`${className} bg-blue-500 shadow-md shadow-blue-300/50 text-white hover:shadow-blue-400/40 active:scale-95 active:shadow-md hover:shadow-lg rounded-xl px-2 py-1 cursor-pointer duration-200  hover:text-white disabled:hover:shadow-none disabled:text-gray-500 disabled:!bg-gray-300 disabled:hover:scale-100 disabled:hover:text-gray-600 disabled:shadow-none disabled:hover:bg-transparent`}
+        className={`${className} bg-blue-500 shadow-md shadow-blue-300/50 بمثط  text-white hover:shadow-blue-400/40 active:scale-95 active:shadow-md hover:shadow-lg rounded-xl px-2 py-1 cursor-pointer duration-200  hover:text-white disabled:hover:shadow-none disabled:text-gray-500 disabled:!bg-gray-300 disabled:hover:scale-100 disabled:hover:text-gray-600 disabled:shadow-none disabled:hover:bg-transparent`}
       >
         <i className="fa-light fa-floppy-disk text-lg"></i>{" "}
-        <span className="tracking-[2.5px] text-lg font-bold">SAVE</span>
+        <span className="tracking-[2.5px] font-bold">SAVE</span>
       </button>
     </div>
   );
