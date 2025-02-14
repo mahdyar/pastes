1a19db56682ca89751ed1ecba70079a2849e7ca7
Sepehr Aghapour <petrol_cs@yahoo.com>
2025-02-14 22:55:08 +0330

    feat: refactor PasswordBox to use form submission for better accessibility

--- src/components/password box/PasswordBox.tsx
+++ src/components/password box/PasswordBox.tsx
@@ -10,7 +10,7 @@ const PasswordBox = ({
   setPassword: React.Dispatch<React.SetStateAction<string>>;
 }) => {
   const [loading, setLoading] = useState<boolean>(false);
-  const onSubmit = async () => {
+  const formOnSubmit = async () => {
     setLoading(true);
     getData();
     setTimeout(() => {
@@ -24,27 +24,32 @@ const PasswordBox = ({
           <img src="/lock.svg" alt="lock" className="w-7 pb-2" />{" "}
           <span className="m-1">Paste is protected!</span>
         </strong>
-        <div>
-          <input
-            type="password"
-            name="slang"
-            minLength={4}
-            maxLength={64}
-            value={password ?? ""}
-            disabled={loading}
-            onChange={(e) => setPassword(e.target.value)}
-            className="invalid:border-red-500 invalid:border disabled:opacity-70 bg-gray-200 shadow-md appearance-none border-2 border-gray-200 rounded-md w-full max-w-64 py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-            placeholder="Enter Password"
-          />
-        </div>
-        <div>
-          <button
-            onClick={onSubmit}
-            className="bg-green-500 shadow-md shadow-green-300/50 text-white hover:shadow-green-400/40 active:scale-95 active:shadow-md hover:shadow-lg rounded-md px-4 py-2 cursor-pointer duration-200  hover:text-white disabled:hover:shadow-none disabled:text-gray-500 disabled:!bg-gray-300 disabled:hover:scale-100 disabled:hover:text-gray-600 disabled:shadow-none disabled:hover:bg-transparent"
-          >
-            Submit
-          </button>
-        </div>
+        <form
+          onSubmit={formOnSubmit}
+          className="flex items-center flex-col gap-5"
+        >
+          <div>
+            <input
+              type="password"
+              name="slang"
+              minLength={4}
+              maxLength={64}
+              value={password ?? ""}
+              disabled={loading}
+              onChange={(e) => setPassword(e.target.value)}
+              className="invalid:border-red-500 invalid:border disabled:opacity-70 bg-gray-200 shadow-md appearance-none border-2 border-gray-200 rounded-md w-full max-w-64 py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+              placeholder="Enter Password"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="bg-green-500 shadow-md shadow-green-300/50 text-white hover:shadow-green-400/40 active:scale-95 active:shadow-md hover:shadow-lg rounded-md px-4 py-2 cursor-pointer duration-200  hover:text-white disabled:hover:shadow-none disabled:text-gray-500 disabled:!bg-gray-300 disabled:hover:scale-100 disabled:hover:text-gray-600 disabled:shadow-none disabled:hover:bg-transparent"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
