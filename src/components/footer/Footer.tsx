84d4240f4e14cc26d0f57759b377f60eee4fac03
Sepehr Aghapour <petrol_cs@yahoo.com>
2025-02-15 00:20:51 +0330

    add github button to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -10,7 +10,19 @@ const Footer = () => {
         <span>{new Date().getFullYear()}</span>
         <span>Pastes.ir</span>
       </div>
-      <div className="flex items-center justify-evenly sm:gap-10 lg:gap-5 sm:justify-end h-full">
+      <div className="flex items-center justify-evenly sm:gap-5 sm:justify-end h-full">
+        <Link to={"https://github.com/mahdyar/pastes"} target="_blank">
+          <button className="cursor-pointer flex items-center gap-2 relative group px-2">
+            <img
+              src="/github.png"
+              alt="Github"
+              className="w-9 md:group-hover:-translate-y-2 duration-200 ease-in opacity-80"
+            />
+            <span className="text-xs bg-black text-white px-1 py-0.5 rounded-sm md:absolute md:-bottom-4 md:left-1/2 md:-translate-x-1/2 duration-200 md:opacity-0 md:group-hover:opacity-100 group-hover:">
+              Github
+            </span>
+          </button>
+        </Link>
         <Link to={"mailto:report@pastes.ir"}>
           <button className="cursor-pointer flex items-center gap-2 relative group px-2">
             <img
@@ -31,7 +43,7 @@ const Footer = () => {
               className="w-9 md:group-hover:-translate-y-2 duration-200 ease-in"
             />
             <span className="text-xs bg-black text-white px-1 py-0.5 rounded-sm md:absolute md:-bottom-4 md:left-1/2 md:-translate-x-1/2 duration-200 md:opacity-0 md:group-hover:opacity-100 group-hover:">
-              telegram
+              Telegram
             </span>
           </button>
         </Link>
